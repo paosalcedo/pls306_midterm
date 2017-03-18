c8e774eb016b439aac80a13a7fbc53cd2c5e7c51
Unknown <paosalcedo@gmail.com>
2017-03-18 02:40:25 -0400

    Added Unity project

---
+++ Code Lab Midterm/Assets/Scripts/FPSController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float gravity = 10.0f;
+	public float maxVelocityChange = 10.0f;
+	public bool canJump = true;
+	public float jumpHeight = 2.0f;
+	private bool grounded = false;
+
+	Rigidbody rb;
+	//
+//	public float sideSpeed;
+//	public float forwardSpeed;
+//	public static float movementSpeed;
+//	public float mouseSensitivity;
+//
+//	float verticalLook = 0; //why declare it here?
+//	float verticalLookMax = 90.0f;
+//	float horizontalLook;
+//
+//	public float verticalVelocity;
+//
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		rb = GetComponent<Rigidbody> ();
+
+		rb.freezeRotation = true;
+		rb.useGravity = false;
+	}
+
+	
+	// Update is called once per frame
+
+	void FixedUpdate()
+	{
+		if (grounded == true || grounded == false) {
+			// Calculate how fast we should be moving
+			Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			targetVelocity = transform.TransformDirection(targetVelocity);
+			targetVelocity *= speed;
+
+			// Apply a force that attempts to reach our target velocity
+			Vector3 velocity = rb.velocity;
+			Vector3 velocityChange = (targetVelocity - velocity);
+			velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+			velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+			velocityChange.y = 0;
+			rb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+			// Jump
+			if (canJump && Input.GetButton ("Jump")) {
+				rb.velocity = new Vector3 (velocity.x, CalculateJumpVerticalSpeed (), velocity.z);
+			} 
+		}
+
+		// We apply gravity manually for more tuning control
+		rb.AddForce(new Vector3 (0, -gravity * rb.mass, 0));
+
+		grounded = false;
+
+
+	}
+
+	void OnCollisionStay () {
+		grounded = true;    
+	}
+
+	float CalculateJumpVerticalSpeed () {
+		// From the jump height and gravity we deduce the upwards speed 
+		// for the character to reach at the apex.
+		return Mathf.Sqrt(2 * jumpHeight * gravity);
+	}
+
+	void Update () 
+	{
+
+
+
+
+
+	}
+}
---
+++ Code Lab Midterm/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	[SerializeField]GameObject crosshair;
+	GameObject enemy;
+	GameObject[] enemies;
+	public float offsetX = 0;
+	public float offsetZ = 0;
+	public float tileZ;
+
+	public string[] fileNames;
+	public static int levelNum = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//looking for all enemies.
+//		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+		Cursor.visible = true;		
+		Cursor.lockState = CursorLockMode.Confined;
+
+		string fileName = fileNames [levelNum];
+
+		string filePath = Application.dataPath + "/" + fileName;
+
+		StreamReader sr = new StreamReader (filePath);
+
+		GameObject levelHolder = new GameObject ("Level Holder");
+
+		int zPos = 0;
+
+		//Read from level text files.
+		while (!sr.EndOfStream) {
+			string line = sr.ReadLine ();
+
+			for (int xPos = 0; xPos < line.Length; xPos++) {
+
+				if (line [xPos] == 'x') {
+					GameObject wall = Instantiate (Resources.Load ("Prefabs/Cube01") as GameObject);
+
+					wall.transform.parent = levelHolder.transform;
+
+					wall.transform.position = new Vector3 (
+						xPos + offsetX, 
+						zPos + offsetZ, 
+						0);
+				}
+			
+				//Instantiate player1
+				if (line [xPos] == 'p') { 
+					GameObject player = Instantiate (Resources.Load ("Prefabs/Player") as GameObject);
+					player.transform.position = new Vector3 (
+						xPos + offsetX, 
+						zPos + offsetZ, 
+						0f);
+				}
+			
+				//Instantiate ice tiles
+			
+			}
+			
+			zPos--;
+		}
+
+		sr.Close();
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		//level changer. aka cheating
+		if(Input.GetKeyDown(KeyCode.P)){
+			levelNum++;
+			SceneManager.LoadScene("test");
+		}
+
+
+	}
+//
+//	void EndLevel ()
+//	{
+//		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+//
+//		foreach (GameObject 
+//	}
+}
